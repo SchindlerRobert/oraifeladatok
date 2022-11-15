912a7ccda38ebdba40d3210f552d31f975425c2d
SchindlerRobert <113940246+SchindlerRobert@users.noreply.github.com>
2022-11-15 10:32:54 +0100

    Create hazi_2.js

---
+++ hazi_2.js
@@ -0,0 +1,15 @@
+Alert(Pelda(1)(2)); 
+
+var pelda = (
+(101, 'peldaert1', 'peldaert2'),
+(100, 'peldaert3', 'peldaert4'),
+(102, 'peldaert5', 'peldaert6'),
+(103, 'peldaert7', 'peldaert8') );
+pelda.pop();
+
+var pelda = (
+(101, 'peldaert9', 'peldaert10'),
+(100, 'peldaert11', 'peldaert12'),
+(102, 'peldaert13', 'peldaert14') );
+pelda.sort();
+console.log(pelda);
